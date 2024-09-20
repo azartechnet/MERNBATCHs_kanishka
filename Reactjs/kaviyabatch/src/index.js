06ad404e65fe698894788f7792dff75a83a610ea
Mohamed Azarudeen <azartechnet@gmail.com>
2024-09-20 16:43:13 +0530

    updated

--- Reactjs/kaviyabatch/src/index.js
+++ Reactjs/kaviyabatch/src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Sample=()=>{
+  return(
+    <div>
+      <h1>This is Heading...</h1>
+    </div>
+  )
+}
+
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<Sample/>);
+
