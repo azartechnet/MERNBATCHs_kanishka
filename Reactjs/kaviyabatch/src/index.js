a349b88dd64242279366be16ef38b8d9980b76cc
Mohamed Azarudeen <azartechnet@gmail.com>
2024-09-21 11:16:44 +0530

    updated

--- Reactjs/kaviyabatch/src/index.js
+++ Reactjs/kaviyabatch/src/index.js
@@ -5,7 +5,25 @@ import ReactDOM from 'react-dom/client';
 const Sample=()=>{
   return(
     <div>
-      <h1>This is Heading...</h1>
+      <nav class="navbar navbar-expand-sm bg-light">
+  <div class="container-fluid">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Register</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
     </div>
   )
 }
