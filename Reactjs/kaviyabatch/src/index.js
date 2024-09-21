4c2eec1c82c551e02228c104f8c7fdfa44b69b41
Mohamed Azarudeen <azartechnet@gmail.com>
2024-09-21 13:18:31 +0530

    Update index.js

--- Reactjs/kaviyabatch/src/index.js
+++ Reactjs/kaviyabatch/src/index.js
@@ -1,33 +1,91 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-
-const Sample=()=>{
+//Basic Rendering
+/*const Sample=()=>{
   return(
     <div>
-      <nav class="navbar navbar-expand-sm bg-light">
-  <div class="container-fluid">
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="#">Home</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Login</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Register</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Contact</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+      <h1>Welcome</h1>
 
     </div>
   )
 }
 
 const r1=ReactDOM.createRoot(document.getElementById('root'))
-r1.render(<Sample/>);
+r1.render(<Sample/>);*/
+
+//Dynamic Rendering
+
+/*const App=()=>{
+  const username="mohamed";
+  return(
+    <div>
+      <h1>{username}</h1>
+      <p>Today date:{new Date().toLocaleDateString()}</p>
+    </div>
+  )
+}
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<App/>)*/
+
+//React without JSX
+
+/*const myelm1=React.createElement('h1',null,'Welcome')
+const r2=ReactDOM.createRoot(document.getElementById('root'))
+r2.render(myelm1)*/
+
+//React with JSX
+
+/*const myelem=<h1>Welcome</h1>
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(myelem)*/
+
+//React List
+
+/*const Myelem=()=>{
+  return(
+    <>
+      <ul>
+        <li>list1</li>
+        <li>list2</li>
+        <li>list3</li>
+      </ul>
+      <ol>
+      <li>list1</li>
+        <li>list2</li>
+        <li>list3</li>
+      </ol>
+    </>
+     
+  )
+}
+
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<Myelem/>)*/
+
+//Conditional Rendering
+//Nested if else,else if ladder,switch statment
+const x=10
+let text=""
+if(x>10)
+{
+  text="goodbye"
+}
+else if(x==10)
+{
+  text="ten"
+}
+else
+{
+  text="x is less than 10"
+}
+const Myelem=()=>{
+  return(
+    <div>
+      <h1>Hello{text}</h1>
+    </div>
+  )
+}
+const r2=ReactDOM.createRoot(document.getElementById('root'))
+r2.render(<Myelem/>)
 
