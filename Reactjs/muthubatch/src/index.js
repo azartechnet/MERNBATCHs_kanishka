5d0ec709c299f2f053fcde5b34cf3edd5e22c02b
Mohamed Azarudeen <azartechnet@gmail.com>
2024-09-26 16:48:13 +0530

    updated

--- Reactjs/muthubatch/src/index.js
+++ Reactjs/muthubatch/src/index.js
@@ -15,7 +15,7 @@ roo.render(<Sample/>)*/
 
 //Dynamic Rendering
 
-const App=()=>{
+/*const App=()=>{
   const username="mohamed";
   return(
     <div>
@@ -26,4 +26,81 @@ const App=()=>{
 }
 
 const r1=ReactDOM.createRoot(document.getElementById('root'))
-r1.render(<App/>)
+r1.render(<App/>)*/
+
+//React without JSX
+
+/*const myelem=React.createElement('h1',null,'Welcome')
+const r2=ReactDOM.createRoot(document.getElementById('root'))
+r2.render(myelem)*/
+
+//React with JSX
+
+/*const myelem=<h1>Welcome</h1>
+
+const r3=ReactDOM.createRoot(document.getElementById('root'))
+r3.render(myelem)*/
+
+//React List
+
+/*const MyElem=()=>{
+  return(
+    <>
+      <ul>
+        <li>list-1</li>
+        <li>list-2</li>
+        <li>list-3</li>
+      </ul>
+      <ol>
+      <li>list-1</li>
+        <li>list-2</li>
+        <li>list-3</li>
+      </ol>
+    </>
+  )
+}
+
+ const r4=ReactDOM.createRoot(document.getElementById('root'))
+ r4.render(<MyElem/>)*/
+
+ //Conditional Rendering
+ //Nested if else,else if ladder,switch stmt
+
+ /*const x=12
+ let text="";
+ if(x>10)
+ {
+  text="goodbye"
+ }
+ else if(x==10)
+ {
+  text="ten"
+ }
+ else
+ {
+  text="hello"
+ }
+ const MyElem=()=>{
+  return( 
+    <div>
+      <h1>{text}</h1>
+    </div>
+  )
+ }
+ const r1=ReactDOM.createRoot(document.getElementById('root'));
+ r1.render(<MyElem/>)*/
+
+ //importing CSS
+
+ import './index.css';
+
+ const App=()=>{
+  return(
+    <>
+       <h1 className='s1'>Welcome</h1>
+    </>
+  )
+ }
+ const r1=ReactDOM.createRoot(document.getElementById('root'));
+
+  r1.render(<App/>)
